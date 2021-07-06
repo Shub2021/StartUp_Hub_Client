6149b006bf7ed2f64d0b989e260af1da09e0103c
Samith Sankalpa <sankalpa.gss@gmail.com>
2021-07-06 11:12:27 +0530

    investor updated

--- screens/Investor/SubscribedStartups.js
+++ screens/Investor/SubscribedStartups.js
@@ -1,97 +1,116 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, FlatList,  } from 'react-native';
-import { Card, Button, IconButton, Colors  } from 'react-native-paper';
-import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import React from "react";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import { Card, Button, IconButton, Colors } from "react-native-paper";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
-const SubscribedStartups=(props)=>{
+const SubscribedStartups = (props) => {
+  const data = [
+    { id: 1, addedDate: "2021-10-12", cType: "Retail Business" },
+    { id: 2, addedDate: "2021-10-12", cType: "Retail Business" },
+    { id: 3, addedDate: "2021-10-12", cType: "Retail Business" },
+    // { id: 4, addedDate: "2021-10-12", cType: "Retail Business" },
+    // { id: 5, addedDate: "2021-10-12", cType: "Retail Business" },
+  ];
+  const startupList = (item) => {
+    return (
+      <Card style={styles.comDetails} kety={item.id}>
+        <View style={styles.allCards}>
+          <Image
+            style={{ width: 80, height: 80, borderRadius: 40, marginTop: 10 }}
+            source={{
+              uri: "https://images.pexels.com/photos/3748221/pexels-photo-3748221.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            }}
+          />
 
-    const data = [
-        {id:1, cDate:'2020-6-18', cType:'Retail Business'},
-        {id:2, cDate:'2020-6-18', cType:'Retail Business'},
-        {id:3, cDate:'2020-6-18', cType:'Retail Business'},
-        {id:4, cDate:'2020-6-18', cType:'Retail Business'},
-        {id:5, cDate:'2020-6-18', cType:'Retail Business'},
-        
-    ]
-    const startupList = ((item)=>{
-        return(
-            <Card style={styles.comDetails} kety={item.id}>
-                <View style={styles.allCards}>
-                    <Image 
-                    style={{width:60, height:60, borderRadius:40, marginTop:10}}
-                    source={{uri:'https://images.unsplash.com/flagged/photo-1578848151039-b8916d7c1c34?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=521&q=80'}}
-                    />
-                    <View style={styles.cardItem}>
-                        <Text>Added Date: {item.cDate}</Text>
-                        <Text>Investment Request: {item.cDate}</Text>
-                    </View>
-                    <Button 
-                    style={{marginTop:50, marginLeft:30}}
-                    onPress={()=>props.navigation.navigate('Notifications')}
-                    color='#0396FF'
-                    >
-                        View Startup
-                </Button>
-                </View>
-                    
-            </Card>
-        )
-    })
+          <View style={{ flexDirection: "row" }}>
+            <View>
+              <Text style={styles.cardTitle}>Added Date</Text>
+              <Text style={{ marginLeft: 25, fontSize: 15 }}>
+                {item.addedDate}
+              </Text>
+            </View>
 
-    return(
-        <View>
-            <FlatList
-                data = {data}
-                renderItem = {({item})=>{
-                  return startupList(item)
-                }}
-                keyExtractor = {item=>`${item.id}`}
-            />
-            
-        </View> 
-    )
-    
-}
+            <View>
+              <Text style={styles.cardTitle}>Startup Type</Text>
+              <Text style={{ marginLeft: 25 }}>{item.cType}</Text>
+            </View>
+          </View>
+        </View>
+        <View
+          style={{
+            padding: 5,
+            marginBottom: 10,
+            marginLeft: 20,
+            marginRight: 40,
+            // width: "100%",
+            // alignItems: "center",
+          }}
+        >
+          <Button
+            icon="information-outline"
+            theme={dtheme}
+            mode="contained"
+            onPress={() => props.navigation.navigate("Notifications")}
+          >
+            View Startup
+          </Button>
+        </View>
+      </Card>
+    );
+  };
+
+  return (
+    <View>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => {
+          return startupList(item);
+        }}
+        keyExtractor={(item) => `${item.id}`}
+      />
+    </View>
+  );
+};
 
 const dtheme = {
-    colors:{
-        primary:'red'
-    }
-}
+  colors: {
+    primary: "#8699fe",
+  },
+};
 const atheme = {
-    colors:{
-        primary:'green'
-    }
-}
+  colors: {
+    primary: "green",
+  },
+};
 
 const styles = StyleSheet.create({
-    comDetails: {
-        margin: 5,
-        marginLeft: 7,
-        marginRight: 7,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.23,
-        shadowRadius: 2.62,
-
-        elevation: 3,     
+  comDetails: {
+    margin: 5,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
 
-    allCards: {
-        flexDirection: 'row',
-        padding: 10,
-    },
+    elevation: 4,
+  },
 
-    cardItem: {
-        marginTop: 20,
-        marginLeft: 20,
-        fontWeight: 'bold', 
-    },
-    
+  allCards: {
+    flexDirection: "row",
+    padding: 20,
+  },
 
-})
+  cardTitle: {
+    marginTop: 20,
+    marginLeft: 20,
+    padding: 5,
+    fontWeight: "bold",
+  },
+});
 
 export default SubscribedStartups;
