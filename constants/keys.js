22f251235a7c83cba0821ed9876dd0b00b925b17
kesh4 <kesharabalasuriya11@gmail.com>
2021-09-16 00:54:13 +0530

    done

--- constants/keys.js
+++ constants/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   jwtkey: "abcd",
-  cn: "http://dbc4-112-134-142-247.ngrok.io",
+  cn: "http://e907-112-134-136-140.ngrok.io",
 };
--- constants/maps.js
+++ constants/maps.js
@@ -1,3 +1,4 @@
 const GOOGLE_API_KEY = "AIzaSyA46Vd8NJcwiW6z";
+// const GOOGLE_API_KEY = "AIzaSyBO5MmzujjM6t-Z_Zmm0BkLYtvuKTwN2c8";
 
 export default GOOGLE_API_KEY;
