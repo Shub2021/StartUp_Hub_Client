443af21ac3b9e5bf457e767439add3703acd61f3
Samith-Sankalpa <sankalpa.gss@gmail.com>
2021-09-20 23:49:50 +0530

    updated

--- constants/keys.js
+++ constants/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   jwtkey: "abcd",
-  cn: "http://d67f-2402-4000-b18d-90b3-d9bf-c23c-64a0-7b0c.ngrok.io",
+  cn: "http://6b3d-2402-4000-b18d-90b3-d9bf-c23c-64a0-7b0c.ngrok.io",
 };
